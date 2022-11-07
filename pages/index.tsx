11a044a380e8c995d67c2ac138aca00179505141
Nouman <noumancodes@gmail.com>
2022-11-07 18:30:17 -0500

    Added link preview

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 export default function Home() {
@@ -13,6 +14,21 @@ export default function Home() {
 
     return (
         <div>
+            <Head>
+                <title>Social Media Preview</title>
+                <meta property="og:url" content="your url" />
+                <meta property="og:type" content="website" />
+                <meta property="fb:app_id" content="your fb app id" />
+                <meta
+                    property="og:title"
+                    content="Social Media Preview Working?"
+                />
+                <meta name="twitter:card" content="summary" />
+                <meta
+                    property="og:description"
+                    content="Hurray!! Yes Social Media Preview is Working"
+                />
+            </Head>
             <input type="date" id="date" />
             <input type="text" id="occasion" />
             <button onClick={handleClick}>Submit Date</button>
