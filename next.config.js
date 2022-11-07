7ee99e1c0d50ab00722178119c18b9e08c55c567
Nouman <noumancodes@gmail.com>
2022-11-07 18:38:49 -0500

    Added rule in next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+    reactStrictMode: true,
+    swcMinify: true,
+};
+module.exports = {
+    trailingSlash: true,
+};
+module.exports = nextConfig;
