fe66f448f14e692820e4ddcc069ba09e9e6c8c5a
Nouman <noumancodes@gmail.com>
2022-11-07 18:07:54 -0500

    Made logic work without styling

---
+++ pages/date/[countdown].tsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/router";
+
+const CountdownPage = () => {
+    const router = useRouter();
+    const { countdown } = router.query;
+
+    const routeArray = String(countdown).split("|");
+
+    const endDate = new Date(routeArray[0] + "T00:00:00");
+    const currentDate = new Date();
+
+    const totalSeconds = (endDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
+
+    // var today = new Date();
+
+    // const timeUntil: String[] = [
+    //     String(parseInt(routeArray[0]) - today.getFullYear()),
+    //     String(parseInt(routeArray[1]) - today.getMonth() - 1),
+    //     String(parseInt(routeArray[2]) - today.getDate()),
+    // ];
+
+    return (
+        <p>
+            {routeArray[1]} is in {days} days {hours} hours {minutes} minutes{" "}
+            {seconds} seconds
+        </p>
+    );
+};
+
+export default CountdownPage;
